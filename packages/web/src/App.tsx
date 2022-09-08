8ef1e2b071c1d2e404c2a395d7471ab73768e0ee
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-09-08 12:42:58 -0700

    Added placeholder content to test PROD deployment

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,6 +5,7 @@ import "destyle.css";
 import { useEffect } from 'react';
 import { ReduxUtils } from '~Utils/ReduxUtils';
 import LoadingWrapper from '~Components/LoadingWrapper/LoadingWrapper';
+import { WindowUtils } from '~Utils/WindowUtils';
 
 function App() {
   useEffect(() => {
@@ -17,6 +18,11 @@ function App() {
     <>
       <LoadingWrapper />
       <Navigation />
+      {Array(100).fill(null).map((_, i) => {
+        return (
+          <h1 key={i} onClick={WindowUtils.toggleScrollLock} style={{ fontSize: "1.5rem" }}>{i}</h1>
+        )
+      })}
     </>
   );
 }
