23bdde651eba2f22662a31f17865281f4d9e41f0
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-09-11 23:40:41 -0700

    cleaned up imports

--- packages/web/src/Pages/GoalsDashboard/GoalsDashboard.tsx
+++ packages/web/src/Pages/GoalsDashboard/GoalsDashboard.tsx
@@ -1,16 +1,4 @@
-import { faArrowTurnDownRight } from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classNames from 'classnames';
-import React, { useEffect, useRef, useState } from 'react'
-import { FormFields, TValidFormField } from '~Components/Form/Form';
-import { GoalBoard } from '~Components/GoalBoard/GoalBoard';
-import { GoalCreationModal } from '~Components/GoalCreationModal/GoalCreationModal';
-import { DangerGradientBtn, DarkToColorGradientBtn, GradientBtn } from '~Components/GradientBtn/GradientBtn';
-import Modal, { ModalProps } from '~FeatureComponents/Modal/Modal';
-import { useAppDispatch, useUserGoals } from '~Store/hooks';
-import { moveGoal, setGoals } from '~Store/slices/UserGoals/UserGoalsSlice';
-import { GoalUtils, TGoal, TGoalCategory } from '~Utils/GoalUtils';
-import { ClassesProp, throttle } from '~Utils/Helpers';
+import { GoalBoard } from "~Components/GoalBoard/GoalBoard";
 import styles from "./GoalsDashboard.module.scss";
 
 type GoalsDashboardProps = {}
