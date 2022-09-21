390e4e86eee9f82709fd9023efb1b6d4d0a981a0
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-09-20 18:32:58 -0700

    All 404 routes should redirect to goals dashboard

--- packages/web/src/Pages/PageNotFound/PageNotFound.tsx
+++ packages/web/src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 type PageNotFoundProps = {}
 
+/** 404 should redirect to user dashboard */
 export default function PageNotFound({}: PageNotFoundProps) {
+
   return (
-	<div>PageNotFound</div>
+    <Navigate to={"/Dashboard"}/>
   )
 }
